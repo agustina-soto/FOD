b27f0248216d68b4280e6f03a480db56f790e218
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-03-29 20:02:47 -0300

    Update ejercicio2.pas

--- ejercicio2.pas
+++ ejercicio2.pas
@@ -1,3 +1,9 @@
+{Realizar un algoritmo que, utilizando el archivo de números enteros no ordenados creados en el ejercicio 1,
+informe por pantalla cantidad de números menores a 1500 y el promedio de los números ingresados.
+
+El nombre del archivo a procesar debe ser proporcionado por el usuario una única vez.
+Además, el algoritmo deberá listar el contenido del archivo en pantalla. }
+
 program ejercicio2;
 const
 	fin = 30000; // lectura limite, no debe incluirse
