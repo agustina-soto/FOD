e3776bfe99cf356ed27494f7ad83b49250e6d527
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-08 12:31:46 -0300

    Update ejercicio4.pas

--- TP1/ejercicio4.pas
+++ TP1/ejercicio4.pas
@@ -1,5 +1,6 @@
 {Agregar al menú del programa del ejercicio 3, opciones para:
-a. Añadir uno o más empleados al final del archivo con sus datos ingresados por teclado. Tener en cuenta que no se debe agregar al archivo un empleado con un número de empleado ya registrado (control de unicidad).
+a. Añadir uno o más empleados al final del archivo con sus datos ingresados por teclado.
+Tener en cuenta que no se debe agregar al archivo un empleado con un número de empleado ya registrado (control de unicidad).
 
 b. Modificar edad a uno o más empleados.
 
