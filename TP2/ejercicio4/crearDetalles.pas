bda269f8fb640fc307b4e57c47c961f131a6b35b
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-11 20:10:27 -0300

    Update crearDetalles.pas

--- TP2/ejercicio4/crearDetalles.pas
+++ TP2/ejercicio4/crearDetalles.pas
@@ -1,23 +1,26 @@
 program crearDetalles;
 type
-	rangoSemana = 1..4;
+	rango = 1..5;
+	rangoDias = 1..30;
 	rangoMes = 1..12; 
 	cadena20 = string[20];
 
 	fecha = record
-		semana: rangoSemana; // random(3)+1;
-		mes: rangoMes; // random(12)+1;
+		dia: rangoDias;
+		mes: rangoMes;
 	end;
 
 	info_detalle = record
 		cod_usuario: integer;
 		fecha: fecha;
-		tiempo_sesion: integer;
+		tiempo: integer;
 	end;
+
+	detalle = file of info_detalle;
 
 
 var
-  archivo1, archivo2, archivo3, archivo4, archivo5: file of info_detalle;
+  archivo1, archivo2, archivo3, archivo4, archivo5: detalle;
   i: integer;
   info_det: info_detalle;
 begin
@@ -26,24 +29,24 @@ begin
   // Crear y cargar datos aleatorios en detalle1
   assign(archivo1, 'detalle_1');
   rewrite(archivo1);
-  for i := 1 to 60 do begin
-    info_det.cod_usuario := i; // de 1 a 60
-    info_det.fecha.semana := random(3)+1; // de 1 a 4
-    info_det.fecha.mes := random(12)+1; // de 1 a 12
-    info_det.tiempo_sesion := random(100)+1; // en horas, de 1 a 100
-    write(archivo1, info_det);
+  for i := 1 to 30 do begin
+	info_det.cod_usuario := i;
+	info_det.fecha.dia := i;
+	info_det.fecha.mes := i;
+	info_det.tiempo := random(20)+1; // en horas, de 1 a 20
+	write(archivo1, info_det);
   end;
   close(archivo1);
 
   // Crear y cargar datos aleatorios en detalle2
   assign(archivo2, 'detalle_2');
   rewrite(archivo2);
-  for i := 1 to 70 do begin
-    info_det.cod_usuario := i; // de 1 a 70
-    info_det.fecha.semana := random(3)+1; // de 1 a 4
-    info_det.fecha.mes := random(12)+1; // de 1 a 12
-    info_det.tiempo_sesion := random(100)+1; // en horas, de 1 a 100
-    write(archivo2, info_det);
+  for i := 10 to 70 do begin
+	info_det.cod_usuario := i;
+	info_det.fecha.dia := i;
+	info_det.fecha.mes := i;
+	info_det.tiempo := random(20)+1; // en horas, de 1 a 20
+	write(archivo2, info_det);
   end;
   close(archivo2);
 
@@ -51,10 +54,10 @@ begin
   assign(archivo3, 'detalle_3');
   rewrite(archivo3);
   for i := 1 to 30 do begin
-    info_det.cod_usuario := i; // de 1 a 30
-    info_det.fecha.semana := random(3)+1; // de 1 a 4
-    info_det.fecha.mes := random(12)+1; // de 1 a 12
-    info_det.tiempo_sesion := random(100)+1; // en horas, de 1 a 100
+    info_det.cod_usuario := i;
+    info_det.fecha.dia := i;
+    info_det.fecha.mes := i;
+    info_det.tiempo := random(20)+1; // en horas, de 1 a 20
     write(archivo3, info_det);
   end;
   close(archivo3);
@@ -62,23 +65,23 @@ begin
   // Crear y cargar datos aleatorios en detalle4
   assign(archivo4, 'detalle_4');
   rewrite(archivo4);
-  for i := 1 to 100 do begin
-    info_det.cod_usuario := i; // de 1 a 100
-    info_det.fecha.semana := random(3)+1; // de 1 a 4
-    info_det.fecha.mes := random(12)+1; // de 1 a 12
-    info_det.tiempo_sesion := random(100)+1; // en horas, de 1 a 100
-    write(archivo4, info_det);
+  for i := 1 to 15 do begin
+	info_det.cod_usuario := i;
+	info_det.fecha.dia := i;
+	info_det.fecha.mes := i;
+	info_det.tiempo := random(20)+1; // en horas, de 1 a 20
+	write(archivo4, info_det);
   end;
   close(archivo4);
 
-// Crear y cargar datos aleatorios en detalle4
+// Crear y cargar datos aleatorios en detalle5
   assign(archivo5, 'detalle_5');
   rewrite(archivo5);
   for i := 1 to 100 do begin
-    info_det.cod_usuario := i; // de 1 a 100
-    info_det.fecha.semana := random(3)+1; // de 1 a 4
-    info_det.fecha.mes := random(12)+1; // de 1 a 12
-    info_det.tiempo_sesion := random(100)+1; // en horas, de 1 a 100
+    info_det.cod_usuario := i;
+    info_det.fecha.dia := i;
+    info_det.fecha.mes := i;
+    info_det.tiempo := random(20)+1; // en horas, de 1 a 20
     write(archivo5, info_det);
   end;
   close(archivo5);
@@ -86,5 +89,4 @@ begin
   writeln('--------------------------');
   writeln('Archivos detalle creados');
   writeln('--------------------------');
-
 end.
