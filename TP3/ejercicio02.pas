058538b5d00eabc5a5900728aab6581e99393004
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-25 02:01:52 -0300

    Update ejercicio02.pas

--- TP3/ejercicio02.pas
+++ TP3/ejercicio02.pas
@@ -8,6 +8,8 @@ Para ello se podrá utilizar algún carácter especial situándolo delante de al
 String a su elección. Ejemplo: ‘@Saldaño’.}
 
 program ejercicio02;
+uses
+	sysutils;
 const
 	CANT_ASISTENTES = 4;
 	NRO_MIN_ASISTENTE = 1000;
@@ -29,14 +31,18 @@ var
 	i: integer;
 	a: asistente;
 begin
+	rewrite(archivo);
 	for i := 1 to CANT_ASISTENTES do begin
+		a.nro_asistente := 997+i;
+		a.dni := IntToStr(i);
+		a.nombre := IntToStr(i+1234) ;
+		{
 		writeln('Ingrese el nro de asistente');
 		readln(a.nro_asistente);
 		writeln('Ingrese el dni');
 		readln(a.dni);
 		writeln('Ingrese el nombre');
 		readln(a.nombre);
-		{
 		writeln('Ingrese el apellido');
 		readln(a.apellido);
 		writeln('Ingrese el email');
@@ -46,6 +52,7 @@ begin
 		}
 		write(archivo, a);
 	end;
+	close(archivo);
 end;
 
 
@@ -54,6 +61,7 @@ procedure bajaLogica(var archivo: asistentes);
 var
 	a: asistente;
 begin
+	reset(archivo);
 	seek(archivo, 0);
 	while (not eof(archivo)) do begin
 		read(archivo, a);
@@ -64,16 +72,42 @@ begin
 			writeln('Se dio de baja un empleado');
 		end;
 	end;
+	close(archivo);
 end;
 
+
+// Carga los datos de un empleado en el archivo de texto traido por parametro
+procedure cargarArchivoTxt(var txt: text; a: asistente);
+begin
+	with a do
+		writeln(txt, ' Nro Asistente: ' , nro_asistente , ' | DNI: ' , dni , ' | Nombre: ' , nombre); // Escribe en el archivo de texto
+end;
+
+
+// Crea un archivo de texto a partir de un archivo binario
+procedure exportarArchivoTxt(var archivo: asistentes; nombreTxt: string);
+var
+	a: asistente;
+	txt: text;
+begin
+	assign(txt, nombreTxt);
+	rewrite(txt);
+	reset(archivo);
+	while (not eof(archivo))do begin
+		read(archivo, a);
+		cargarArchivoTxt(txt, a);
+	end;
+	writeln('Se cargo el archivo de texto');
+	close(txt); close(archivo);
+end;
 
 // ---------------------------------------------------------------------
 var
 	archivo: asistentes;
 begin
 	assign(archivo, 'asistentes');
-	rewrite(archivo);
 	cargarDatos(archivo);
+	exportarArchivoTxt(archivo, 'asistentes_congreso_completo.txt');
 	bajaLogica(archivo);
-	close(archivo);
+	exportarArchivoTxt(archivo, 'asistentes_congreso_con_bajas.txt');
 end.
