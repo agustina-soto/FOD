c2faa634c49f8dc062deac15ecf87765c77566d4
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-28 02:15:03 -0300

    Update crear_maestro_y_detalle.pas

--- TP3/Ejercicio06/crear_maestro_y_detalle.pas
+++ TP3/Ejercicio06/crear_maestro_y_detalle.pas
@@ -1,6 +1,8 @@
 program crear_maestro_y_detalle;
 uses
 	sysutils;
+const
+	DIMF = 20;
 type
 	cadena50 = string[50];
 	cadena100 = string[100];
@@ -22,7 +24,7 @@ type
 
 	detalle = file of info_det;
 
-	codigosAsignados = array[0..999] of boolean;
+	codigosAsignados = array[0..DIMF] of boolean;
 
 
 
@@ -31,13 +33,13 @@ procedure inicializarCodigosOcupados(var v: codigosAsignados);
 var
 	i: integer;
 begin
-	for i := 0 to 999 do
+	for i := 0 to DIMF do
 		v[i] := false;
 end;
 
 
 // Carga un archivo maestro con datos aleatorios no ordenados
-procedure cargarMaestro(var mae: maestro; v: codigosAsignados);
+procedure cargarMaestro(var mae: maestro; var v: codigosAsignados);
 var
 	i: integer;
 	reg: info_mae;
@@ -45,9 +47,9 @@ begin
 	randomize; // Inicializar el generador de números aleatorios
 
 	// Generar y escribir registros aleatorios en el archivo maestro
-	for i := 1 to 100 do begin
+	for i := 1 to 8 do begin
 		repeat
-			reg.cod := random(1000); // Generar un número aleatorio UNICO entre 0 y 999 para el código
+			reg.cod := random(20)+1; // Generar un número aleatorio UNICO entre 1 y 20 para el código
 		until not v[reg.cod];
 	
 		// Marcar el código como asignado
@@ -55,7 +57,7 @@ begin
 		reg.descripcion := 'Prenda ' + IntToStr(i); // Descripción aleatoria
 		reg.colores := 'Rojo, Azul, Verde'; // Colores aleatorios
 		reg.tipo_prenda := 'Tipo ' + IntToStr(random(15)+1); // Tipo aleatorio entre 1 y 15
-		reg.stock := random(100); // Stock aleatorio entre 0 y 99
+		reg.stock := random(130); // Stock aleatorio entre 0 y 99
 		reg.precio_unitario := random(10000) / 10; // Precio aleatorio entre 0 y 99.9
 
 		write(mae, reg); // Escribir el registro en el archivo maestro
@@ -66,14 +68,18 @@ end;
 // Carga un archivo detalle a partir de los codigos asignados por el maestro
 procedure cargarDetalle(var det: detalle; v: codigosAsignados);
 var
-	i: integer;
+	pos, cant: integer;
 	reg: info_det;
 begin
-	for i := 1 to 300 do
-		if (v[i]) then begin // Si el codigo esta ocupado (osea que el maestro lo asignó)
-			reg.cod := i;
+	pos := 1; cant := 0;
+	while ( (pos < DIMF) AND (cant < 4) ) do begin // cant < 4 porque quiero hasta 4 actualizaciones para el maestro
+		if (v[pos]) then begin // Si el codigo esta ocupado (osea que el maestro lo asignó)
+			reg.cod := pos;
 			write(det, reg);
+			cant := cant + 1;
 		end;
+		pos := pos + 1;
+	end;
 end;
 
 // ---------------------------------------------------------------------
