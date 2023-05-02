f3886060bc9c282ebdfa18bcd40c47d335975b84
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-05-02 16:46:43 -0300

    Update ejercicio03.pas

--- TP3/ejercicio03.pas
+++ TP3/ejercicio03.pas
@@ -1,31 +1,3 @@
-{Realizar un programa que genere un archivo de novelas filmadas durante el presente año.
-De cada novela se registra: código, género, nombre, duración, director y precio.
-El programa debe presentar un menú con las siguientes opciones:
-
-a. Crear el archivo y cargarlo a partir de datos ingresados por teclado. Se utiliza la técnica de lista invertida para recuperar
-espacio libre en el archivo. Para ello, durante la creación del archivo, en el primer registro del mismo se debe almacenar la cabecera
-de la lista. Es decir un registro ficticio, inicializando con el valor cero (0) el campo correspondiente al código de novela, el cual
-indica que no hay espacio libre dentro del archivo.
-
-b. Abrir el archivo existente y permitir su mantenimiento teniendo en cuenta el inciso a., se utiliza lista invertida para recuperación
-de espacio. En particular, para el campo de  ́enlace ́ de la lista, se debe especificar los números de registro referenciados con signo negativo,
-(utilice el código de novela como enlace).Una vez abierto el archivo, brindar operaciones para:
-
-i. Dar de alta una novela leyendo la información desde teclado. Para esta operación, en caso de ser posible, deberá recuperarse el espacio libre.
-Es decir, si en el campo correspondiente al código de novela del registro cabecera hay un valor negativo, por ejemplo -5, se debe leer el registro
-en la posición 5, copiarlo en la posición 0 (actualizar la lista de espacio libre) y grabar el nuevo registro en la posición 5.
-Con el valor 0 (cero) en el registro cabecera se indica que no hay espacio libre.
-
-ii. Modificar los datos de una novela leyendo la información desde teclado. El código de novela no puede ser modificado.
-
-iii. Eliminar una novela cuyo código es ingresado por teclado. Por ejemplo, si se da de baja un registro en la posición 8,
-en el campo código de novela del registro cabecera deberá figurar -8, y en el registro en la posición 8 debe copiarse el antiguo registro cabecera.
-
-c. Listar en un archivo de texto todas las novelas, incluyendo las borradas, que representan la lista de espacio libre.
-El archivo debe llamarse “novelas.txt”.
-NOTA: Tanto en la creación como en la apertura el nombre del archivo debe ser proporcionado por el usuario.
-}
-
 program ejercicio3;
 const
 	VALOR_ALTO = -1;
@@ -73,8 +45,6 @@ var
 	n: novela;
 begin
 	n.cod := 0; n.genero := '...'; n.nombre := '...'; n.duracion := 0; n.director := '...'; n.precio := 0; // Cabecera del archivo
-	// tengo que inicializar todos o solo con el codigo alcanza?
-
 	while (n.cod <> VALOR_ALTO) do begin
 		write(archivo, n);
 		leerNovela(n);
@@ -97,7 +67,7 @@ end;
 
 
 // Recibe un archivo y un numero de novela y devuelve si existe en el archivo
-procedure existeNovela(var archivo: novelas; var n: novela; var existe: boolean); // "n" llega con el cod de novela a buscar, cuando existe = true en "n" queda la info del registro
+procedure existeNovela(var archivo: novelas; var n: novela; var existe: boolean); // "n" llega con el cod de novela a buscar, cuando existe = true en "e" queda la info del registro
 var
 	num: integer;
 begin
@@ -205,9 +175,15 @@ begin
 		else begin
 			seek(archivo, cabecera.cod*-1); // Se posiciona en el primer espacio libre
 			read(archivo, aux); // Leo el contenido que hay en la posicion sobre la que voy a insertar el nuevo elemento
+			// seek(archivo, filePos(archivo)-1)
+			// write(archivo, n)
 			seek(archivo, 0); // Vuelvo a la cabecera para actualizarla
 			write(archivo, aux); // Escribe el nuevo primer espacio libre
+			
 			seek(archivo, cabecera.cod*-1);
+			// en vez de hacer este seek y el write de abajo lo puedo hacer como los comentarios que estan 2 filas antes
+			// me lo dijo juani asiq todo legal :)
+			
 			write(archivo, n);
 
 			writeln('Se ha agregado una novela');
